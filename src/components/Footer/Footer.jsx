7fd9f18a247307aba54c3ffba298fda8a60ea3d4
Ajinkyafulpatil <ajinkyafulpatil@gmail.com>
2024-08-22 11:22:02 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+        <p>Copyright Ⓒ All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
